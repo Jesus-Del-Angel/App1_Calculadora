b44dc95e07a01c9e3326d234f7eb6fec3e9d324f
Jesus-Del-Angel <jesus.delangel@iest.edu.mx>
2026-01-30 08:24:50 -0600

    Se añadió la opción de suma

---
+++ proyecyo4/proyecyo4/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Boton_Click(sender As Object, e As EventArgs) Handles Boton.Click
+        Dim Numero1 As Integer = Integer.Parse(Caja1.Text)
+        Dim Numero2 As Integer = Integer.Parse(Caja2.Text)
+        Dim Resultado As Integer = Numero1 + Numero2
+        Etiqueta4.Text = Resultado.ToString()
+    End Sub
+End Class
